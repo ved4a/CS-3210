ec4ed910066bdcf2342b3cea299d76121953c21d
Vedaa Anand <work.vedaa@gmail.com>
2025-03-12 15:17:29 +0530

    feat: read coordinates from input file and store

--- PA-1/main.cpp
+++ PA-1/main.cpp
@@ -3,3 +3,20 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+// Read coordinates from input.txt
+vector<pair<int, int>> readCoordinates(const string& filename) {
+    ifstream infile(filename);
+
+     // Read the first line to get # of points
+    int n;
+    infile >> n;
+
+    // Declare a vector pair to store coordinates post-reading
+    vector<pair<int, int>> points(n);
+    for(int i = 0; i < n; i++){
+        infile >> points[i].first >> points[i].second;
+    }
+    infile.close();
+    return points;
+}
