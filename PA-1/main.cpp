68b406f340b368bba65836a871f2fbe79b1132b5
Vedaa Anand <work.vedaa@gmail.com>
2025-03-12 15:21:47 +0530

    feat: check if print function works

--- PA-1/main.cpp
+++ PA-1/main.cpp
@@ -5,18 +5,32 @@
 using namespace std;
 
 // Read coordinates from input.txt
-vector<pair<int, int>> readCoordinates(const string& filename) {
+vector<pair<int, int>> readInput(const string& filename) {
     ifstream infile(filename);
 
      // Read the first line to get # of points
     int n;
     infile >> n;
 
-    // Declare a vector pair to store coordinates post-reading
+    // Declare a vector pair of size n to store coordinates post-reading
     vector<pair<int, int>> points(n);
     for(int i = 0; i < n; i++){
         infile >> points[i].first >> points[i].second;
     }
+
     infile.close();
     return points;
+}
+
+int main(void) {
+    string inputFile = "./input.txt";
+    vector<pair<int, int>> points = readInput(inputFile);
+
+    // Print the points to check output
+    cout << "Read " << points.size() << " points from " << inputFile << ":\n";
+    for (const auto& p : points) {
+        cout << "(" << p.first << ", " << p.second << ")\n";
+    }
+
+    return 0;
 }
