2faa7bfc52ed77b9e86e254cfc35a6d9e9b7c8a3
Vedaa Anand <work.vedaa@gmail.com>
2025-03-15 20:00:43 +0530

    feat: create structure for comparison instead of manually re-typing

--- PA-1/main.cpp
+++ PA-1/main.cpp
@@ -7,3 +7,12 @@ using namespace std;
 
 // A point is a pair of integers
 typedef pair<int, int> Point;
+
+// Comparator for sorting points
+// Sort by descending x-coordinate first
+// If x-coordinates are equal, sort by ascending y-coordinate
+struct CompareX {
+    bool operator()(const Point &a, const Point &b) const {
+        return a.first > b.first || (a.first == b.first && a.second < b.second);
+    }
+};
